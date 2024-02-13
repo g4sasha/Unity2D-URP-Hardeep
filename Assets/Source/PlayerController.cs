d98bebeef6a9c267ac9f801a544f252220147bdb
Alexandr Gyach <as.gyach@gmail.com>
2024-02-14 00:15:25 +0300

    working progress

--- Assets/Source/PlayerController.cs
+++ Assets/Source/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,5 +12,13 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0f, 0f, _rotationSpeed * Time.deltaTime * -1f);
         transform.position += Vector3.down * (_fallSpeed * Time.deltaTime);
         transform.position += new Vector3(Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime, 0f, 0f);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
